9e8402af58fd0a656d443275e25e0a131a7302e4
Hasini Ruwanthika <hasinirsoorige9845@gmail.com>
2026-09-15 10:57:45 +0530

    Configure API services and Swagger

--- Program.cs
+++ Program.cs
@@ -1,23 +1,38 @@
+using LiquidlabsPostcacheApi.ExternalApi;
+using LiquidlabsPostcacheApi.Middleware;
+using LiquidlabsPostcacheApi.Repositories;
+using LiquidlabsPostcacheApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Add services
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Application DI Registrations
+builder.Services.AddScoped<IPostRepository, PostRepository>();
+builder.Services.AddScoped<IPostService, PostService>();
+
+// Typed HttpClient Registration
+builder.Services.AddHttpClient<IJsonPlaceholderClient, JsonPlaceholderClient>(c =>
+{
+    c.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
+    c.Timeout = TimeSpan.FromSeconds(10);
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure HTTP pipeline
+app.UseMiddleware<ExceptionMiddleware>();
+
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
